b83dbd72ceeb9e1a4d3fb25242f3e15d83295cd0
natalianavhur <nnavhur@gmail.com>
2024-09-24 10:48:52 -0600

    Prepare C# Part 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string first_name, last_name;
+
+        Console.Write("What is your first name? ");
+        first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
+        Console.Write("What is your grade percentage?>");
+        int grade = int.Parse(Console.ReadLine()); 
+        string letter;
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >=80){
+            letter = "B";
+        }
+        else if (grade >=70) {
+            letter = "C";
+        }
+        else if (grade >=60) {
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (grade >=70){
+            Console.WriteLine($"Congratulations! {first_name} {last_name} passed the couse.");
+        }
+        else{
+            Console.WriteLine("Good Luck next time!");
+        }
     }
+    
 }
