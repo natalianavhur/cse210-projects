08784012b314df54135491ba64981d136f733729
natalianavhur <nnavhur@gmail.com>
2024-09-24 10:50:21 -0600

    Prepare C# Part 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         string first_name, last_name;
 
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         first_name = Console.ReadLine();
 
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         last_name = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
